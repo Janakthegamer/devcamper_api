924d77ac7295c0ed1d10fec5c747bd3648c46df3
Janak <janakthegamer@gmail.com>
2022-02-12 23:39:56 +0530

    Get Single Review & Update Seeder

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,7 +1,5 @@
 const express = require("express");
-const {
-    getReviews
-} = require("../controllers/reviews")
+const { getReviews, getReview } = require("../controllers/reviews")
 
 const Review = require("../models/Review");
 
@@ -10,14 +8,14 @@ const router = express.Router({ mergeParams: true });
 const advancedResults = require("../middlewares/advancedResults");
 const { protect, authorize } = require("../middlewares/auth");
 
-router
-    .route("/")
-    .get(
+router.route("/").get(
         advancedResults(Review, {
             path: "bootcamp",
             select: "name description",
         }),
         getReviews
-    )
+    );
+
+router.route("/:id").get(getReview);
 
 module.exports = router;
--- seeder.js
+++ seeder.js
@@ -26,6 +26,7 @@ dotenv.config({ path: "./config/config.env" });
 const Bootcamp = require("./models/Bootcamp");
 const Course = require("./models/Course");
 const User = require("./models/User");
+const Review = require("./models/Review");
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -37,12 +38,13 @@ mongoose.connect(process.env.MONGO_URI, {
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8"))
 const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8"))
 const users = JSON.parse(fs.readFileSync(`${__dirname}/_data/users.json`, "utf-8"))
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/_data/reviews.json`, "utf-8"))
 
 
 // Import into DB
-const importData = async (model, name, type) => {
+const importData = async (model, name, data) => {
     try {
-        await model.create(type);
+        await model.create(data);
 
         console.log(`${name} Data Imported...`.green.inverse);
         process.exit();
@@ -63,6 +65,36 @@ const deleteData = async (model, name) => {
     }
 }
 
+// Import everything into DB
+const importAll = async (name, data) => {
+    try {
+        await Bootcamp.create(bootcamps);
+        await Course.create(courses);
+        await User.create(users);
+        await Review.create(reviews);
+
+        console.log(`All Data Imported...`.green.inverse);
+        process.exit();
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+// Delete all data
+const deleteAll = async (model) => {
+    try {
+        await Bootcamp.deleteMany();
+        await Course.deleteMany();
+        await User.deleteMany();
+        await Review.deleteMany();
+
+        console.log(`All Data Destroyed...`.red.inverse);
+        process.exit();
+    } catch (err) {
+        console.log(err);
+    }
+}
+
 // Error
 const showError = async () => {
     console.log("Error".red.inverse + " Check command again");
@@ -71,20 +103,34 @@ const showError = async () => {
 
 if (process.argv[2] === "-i" && process.argv[3] === "course") {
     importData(Course, "Course", courses);
+
 } else if (process.argv[2] === "-d" && process.argv[3] === "course") {
     deleteData(Course, "Course");
+
 } else if (process.argv[2] === "-i" && process.argv[3] === "bootcamp") {
     importData(Bootcamp, "Bootcamp", bootcamps);
+
 } else if (process.argv[2] === "-d" && process.argv[3] === "bootcamp") {
     deleteData(Bootcamp, "Bootcamp");
+
 } else if (process.argv[2] === "-i" && process.argv[3] === "user") {
     importData(User, "User", users);
+
 } else if (process.argv[2] === "-d" && process.argv[3] === "user") {
-    deleteData(User, "User", users);
+    deleteData(User, "User");
+
 } else if (process.argv[2] === "-i" && process.argv[3] === "review") {
-    showError();
+    importData(Review, "Review", reviews);
+
 } else if (process.argv[2] === "-d" && process.argv[3] === "review") {
-    showError();
+    deleteData(Review, "Review");
+
+} else if (process.argv[2] === "-i" && process.argv[3] === "all") {
+    importAll();
+
+} else if (process.argv[2] === "-d" && process.argv[3] === "all") {
+    deleteAll();
+
 } else {
     showError();
 }
